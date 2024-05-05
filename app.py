a6acfef1879767daee719518b716a0328426686e
rakibhasansazib <154662882+rakibhasansazib@users.noreply.github.com>
2024-05-05 14:19:13 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Congrates!!! you deployed apps on k8s successfully.'
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
