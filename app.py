2beef163f028d02e896eb22cd1affb62e8f29c90
rakibhasansazib <154662882+rakibhasansazib@users.noreply.github.com>
2024-05-07 12:50:32 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'GOAT(Rakib).'
+    return 'Apps Deployed Successfully!!!.'
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
